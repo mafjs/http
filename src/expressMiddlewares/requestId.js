856deefd6c306e58b6cf0ab46a51a51f0b8bc9ce
alekzonder <alekzonder@gmail.com>
2017-08-19 01:35:22 +0700

    chg: dont set req.id if already exists

--- src/expressMiddlewares/requestId.js
+++ src/expressMiddlewares/requestId.js
@@ -1,7 +1,10 @@
 var uuid = require('uuid/v4');
 
 module.exports = function (req, res, next) {
-    req.id = uuid();
-    res.set('X-Request-Id', req.id);
+    if (!req.id) {
+        req.id = uuid();
+        res.set('X-Request-Id', req.id);
+    }
+
     next();
 };
